95f639a3516ad2d152d789f2c5cebbc60be28916
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-07-01 17:28:23 +0800

    Updates

---
+++ assets/layout-CIed_Wrq.js
@@ -0,0 +1,32 @@
+import{X as e,Z as t,gt as n,vt as r,xt as i}from"./index-hckVvsFr.js";import{t as a}from"./LogoTangy-Dtrdj_Ia.js";var o=`/tangy/assets/loginBgi-DtjVt9SS.png`,s=`/tangy/assets/loginBgiSm-6S_joEMi.png`,c=i.img`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  object-fit: cover;
+
+  z-index: -1;
+`,l=i.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 0 16px;
+  ${t.lg} {
+    padding: 0;
+  }
+`;i.img`
+  max-width: 400px;
+  width: 100%;
+  display: none;
+  object-fit: cover;
+  ${t.sm} {
+    display: block;
+  }
+`;var u=i(e)`
+  width: 100%;
+  ${t.sm} {
+    width: 50%;
+  }
+`,d=n();function f(){return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsxs)(`picture`,{children:[(0,d.jsx)(`source`,{media:`(max-width: 869px)`,srcSet:s}),(0,d.jsx)(c,{src:o,alt:``})]}),(0,d.jsx)(l,{children:(0,d.jsx)(e,{style:{width:`100%`,height:`100vh`},$justify:`flex-end`,$gap:`lg`,children:(0,d.jsx)(u,{$justify:`center`,$align:`center`,children:(0,d.jsxs)(e,{style:{width:`352px`},$direction:`column`,children:[(0,d.jsx)(e,{style:{width:`100%`},children:(0,d.jsx)(a,{type:!0})}),(0,d.jsx)(r,{})]})})})})]})}export{f as LoginLayout};
