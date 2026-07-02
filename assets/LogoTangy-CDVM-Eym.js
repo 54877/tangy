fa4e3b8c1b333dea45433692d5858f0d3334a521
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-07-02 15:09:16 +0800

    Updates

---
+++ assets/LogoTangy-CDVM-Eym.js
@@ -0,0 +1,27 @@
+import{Tt as e,W as t,at as n,it as r,kt as i,rt as a,wt as o}from"./index-DB4eQwgJ.js";var s=`/tangy/assets/logo2-C32vIXhK.png`,c=i.img`
+  width: 80px;
+  height: 80px;
+  position: absolute;
+  object-fit: cover;
+
+  transform: ${({$type:e})=>e?`translate(-14%, -45px)`:`translate(-110%, -5px)`};
+
+  ${n.sm} {
+    transform: translate(-110%, -5px);
+  }
+`,l=i(t)`
+  && {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 1;
+    color: #232529;
+  }
+`,u=i(r)`
+  margin: 0 0 0 -18px;
+`,d=i.div({display:`flex`,alignItems:`center`,position:`relative`}),f=i(r)`
+  padding-left: ${({$type:e})=>e?`20px`:`67px`};
+
+  ${n.sm} {
+    padding-left: 67px;
+  }
+`,p=o();function m({type:t=!1}){return(0,p.jsx)(e,{to:`/`,children:(0,p.jsx)(f,{$type:t,children:(0,p.jsxs)(d,{children:[(0,p.jsx)(c,{$type:t,src:s,alt:`Tangy Logo`}),(0,p.jsxs)(u,{$type:t,$direction:`column`,$align:`center`,$justify:`center`,$gap:`none`,children:[(0,p.jsx)(l,{variant:`inherit`,children:`碳吉學院`}),(0,p.jsx)(a,{$size:`xs`,children:`TANJI ACADEMY`})]})]})})})}export{m as t};
---
+++ assets/layout-BZ_mtw7I.js
@@ -0,0 +1,32 @@
+import{Et as e,at as t,it as n,kt as r,wt as i}from"./index-DB4eQwgJ.js";import{t as a}from"./LogoTangy-CDVM-Eym.js";var o=`/tangy/assets/loginBgi-DtjVt9SS.png`,s=`/tangy/assets/loginBgiSm-6S_joEMi.png`,c=r.img`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  object-fit: cover;
+
+  z-index: -1;
+`,l=r.div`
+  max-width: 1320px;
+  margin: 0 auto;
+  padding: 0 16px;
+  ${t.lg} {
+    padding: 0;
+  }
+`;r.img`
+  max-width: 400px;
+  width: 100%;
+  display: none;
+  object-fit: cover;
+  ${t.sm} {
+    display: block;
+  }
+`;var u=r(n)`
+  width: 100%;
+  ${t.sm} {
+    width: 50%;
+  }
+`,d=i();function f(){return(0,d.jsxs)(d.Fragment,{children:[(0,d.jsxs)(`picture`,{children:[(0,d.jsx)(`source`,{media:`(max-width: 869px)`,srcSet:s}),(0,d.jsx)(c,{src:o,alt:``})]}),(0,d.jsx)(l,{children:(0,d.jsx)(n,{style:{width:`100%`,height:`100vh`},$justify:`flex-end`,$gap:`lg`,children:(0,d.jsx)(u,{$justify:`center`,$align:`center`,children:(0,d.jsxs)(n,{style:{width:`352px`},$direction:`column`,children:[(0,d.jsx)(n,{style:{width:`100%`},children:(0,d.jsx)(a,{type:!0})}),(0,d.jsx)(e,{})]})})})})]})}export{f as LoginLayout};
