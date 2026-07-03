491726815c428c78b99fbe48dc6788cb97024df8
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-07-03 14:27:33 +0800

    Updates

---
+++ assets/LogoTangy-I_7dfIzJ.js
@@ -0,0 +1,27 @@
+import{At as e,Ft as t,dt as n,jt as r,lt as i,q as a,ut as o}from"./index-DnqiyR1q.js";var s=`/tangy/assets/logo2-C32vIXhK.png`,c=t.img`
+  width: 80px;
+  height: 80px;
+  position: absolute;
+  object-fit: cover;
+
+  transform: ${({$type:e})=>e?`translate(-14%, -45px)`:`translate(-110%, -5px)`};
+
+  ${n.sm} {
+    transform: translate(-110%, -5px);
+  }
+`,l=t(a)`
+  && {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 1;
+    color: #232529;
+  }
+`,u=t(o)`
+  margin: 0 0 0 -18px;
+`,d=t.div({display:`flex`,alignItems:`center`,position:`relative`}),f=t(o)`
+  padding-left: ${({$type:e})=>e?`20px`:`67px`};
+
+  ${n.sm} {
+    padding-left: 67px;
+  }
+`,p=e();function m({type:e=!1}){return(0,p.jsx)(r,{to:`/`,children:(0,p.jsx)(f,{$type:e,children:(0,p.jsxs)(d,{children:[(0,p.jsx)(c,{$type:e,src:s,alt:`Tangy Logo`}),(0,p.jsxs)(u,{$type:e,$direction:`column`,$align:`center`,$justify:`center`,$gap:`none`,children:[(0,p.jsx)(l,{variant:`inherit`,children:`碳吉學院`}),(0,p.jsx)(i,{$size:`xs`,children:`TANJI ACADEMY`})]})]})})})}export{m as t};
