86f29798d10222519a3145230cbdc770b35adc5c
XiangXiang <Xiang_Huang@genesis.com.tw>
2026-08-18 17:01:10 +0800

    新增登出全部裝置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from "vite";
-
+import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
+
 export default defineConfig({
+  plugins: [react()],
   base: "/tangy/",
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "https://tangy-api.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
